fc89401e6cd391cb340e1ab39b3b37bc49aea4a7
Wilfredo Diaz <wdiazdev@gmail.com>
2022-12-18 14:17:13 -0500

    onClick navbar event

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,11 +13,11 @@ export default function Navbar() {
         <nav className='navbar--container'>
             <h3>WD</h3>
             <div className={click ? 'nav--menu active' : 'nav--menu'}>
-                <a className='focus-in-expand' href='#home'>Home</a>
-                <a className='focus-in-expand' href='#about'>About</a>
-                <a className='focus-in-expand' href='#skills'>Skills</a>
-                <a className='focus-in-expand' href='#work'>Work</a>
-                <a className='focus-in-expand' href='#contact'>Contact</a>
+                <a onClick={handleClick} className='focus-in-expand' href='#home'>Home</a>
+                <a onClick={handleClick} className='focus-in-expand' href='#about'>About</a>
+                <a onClick={handleClick} className='focus-in-expand' href='#skills'>Skills</a>
+                <a onClick={handleClick} className='focus-in-expand' href='#work'>Work</a>
+                <a onClick={handleClick} className='focus-in-expand' href='#contact'>Contact</a>
             </div>
             <div className='hamburger--menu' onClick={handleClick}>
                 {click ? (<FaTimes size={35} />) : (<FaBars size={35} />)}
