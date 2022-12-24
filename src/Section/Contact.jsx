edd75eb9aa222f0bde26db73769fe633d4366c95
Wilfredo Diaz <wdiazdev@gmail.com>
2022-12-24 14:41:44 -0500

    contact

--- src/Section/Contact.jsx
+++ src/Section/Contact.jsx
@@ -1,10 +1,32 @@
+import { useRef, useState } from 'react';
 import '../Styles/Contact.css';
 import Header from '../Components/Header';
 import { FaPhone, FaMapMarkerAlt, FaEnvelope } from 'react-icons/fa';
-import * as FaIcons from 'react-icons/fa';
-import * as SiIcons from "react-icons/si";
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+
+    const [sent, setSent] = useState(false);
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_s6vj00j', 'template_x9wlhdd', e.target, '5Ov3pj3qqAysxs7SO')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+        setSent(true);
+    };
+
+    setTimeout(() => {
+        setSent(false)
+    }, 5000)
+
     return (
         <div className='contact' id='contact'>
 
@@ -37,6 +59,35 @@ export default function Contact() {
                         <span>407-437-8084</span>
 
                     </div>
+
+                </div>
+
+
+                <div className='form--container'>
+
+                    <form ref={form} onSubmit={sendEmail}>
+
+                        <label>Name</label>
+                        <input type='text' name='from_name' required />
+
+                        <label>Email</label>
+                        <input type='email' name='user_email' required />
+
+                        <label>Subject</label>
+                        <input type='text' name='subject' required />
+
+                        <label>Message</label>
+                        <textarea name='message' required />
+
+                        <input type='submit' value='Send' />
+
+                    </form>
+
+                    {sent ?
+                        <p className='msg--sent'>
+                            The message has been sent!
+                        </p> : null
+                    }
 
                 </div>
 
