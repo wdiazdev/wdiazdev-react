16694df151ec7b4394c540580e7881927a5eef0c
Wilfredo Diaz <wdiazdev@gmail.com>
2022-12-26 20:32:04 -0500

    Contact btn

--- src/Section/Contact.jsx
+++ src/Section/Contact.jsx
@@ -83,7 +83,7 @@ export default function Contact() {
                         <textarea name='message' required='required' placeholder='' />
                     </div>
 
-                    <input type='submit' value='Send' className='btn' />
+                    <input type='submit' value='Send' className='contact--btn' />
 
                     {sent ?
                         <p className='msg--sent tracking-in-expand-fwd-top'>
