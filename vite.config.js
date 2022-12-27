3a6ac091d6287367694e6ae0804fcfe26b73e852
Wilfredo Diaz <wdiazdev@gmail.com>
2022-12-26 22:50:24 -0500

    url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wdiazdev.com/",
+  base: "/wdiazdev-react/",
   plugins: [react()],
 })
