d188a33f941a085c57e82ff27bbfb0d99decf513
Wilfredo Diaz <wdiazdev@gmail.com>
2023-08-03 20:11:05 -0400

    Styles update for icons and welcome page

--- src/Section/Projects.jsx
+++ src/Section/Projects.jsx
@@ -1,58 +1,39 @@
-import Header from '../Components/Header';
-import '../Styles/Projects.css';
-import WorkData from '../Data/WorkData';
-import { FaCode } from 'react-icons/fa';
+import Header from "../Components/Header"
+import "../Styles/Projects.css"
+import WorkData from "../Data/WorkData"
+import { FaCode } from "react-icons/fa"
 
 export default function Work() {
-    return (
-        <div className='work' id='projects'>
+  return (
+    <div className="work" id="projects">
+      <Header header={"My Work"} subHeader={"Some things I've built."} />
 
-            <Header header={'My Work'}
-                subHeader={"Some things I've built."} />
+      <div className="card--container" data-aos="fade-up" data-aos-duration="1000">
+        {WorkData.map((item) => {
+          return (
+            <div key={item.id} className="card">
+              <img src={item.img} alt="Card" />
 
-            <div className='card--container' data-aos="fade-up" data-aos-duration='1000'>
+              <div className="card--info">
+                <h3>{item.name}</h3>
 
-                {
-                    WorkData.map((item) => {
-                        return (
-                            <div key={item.id} className='card'>
+                <span>{item.tech}</span>
 
-                                <img src={item.img} alt='Card' />
+                <p>{item.description}</p>
 
-                                <div className='card--info'>
-
-
-                                    <h3>{item.name}</h3>
-
-                                    <span>{item.tech}</span>
-
-                                    <p>{item.description}</p>
-
-                                    <div className='work--icons'>
-                                        <a
-                                            href={item.projectUrl}
-                                            target='_blank'
-                                            className='card--icon'
-                                        >
-                                            <FaCode />
-                                        </a>
-                                        <a
-                                            href={item.ghPagesUrl}
-                                            target='_blank'
-                                            className='card--icon--live'
-                                        >
-                                            LIVE
-                                        </a>
-                                    </div>
-
-                                </div>
-
-                            </div>
-                        )
-                    })
-                }
-
-            </div >
-        </div >
-    )
+                <div className="work--icons">
+                  <a href={item.projectUrl} target="_blank" className="card--icon">
+                    <FaCode />
+                  </a>
+                  <a href={item.ghPagesUrl} target="_blank" className="card--icon--live">
+                    LIVE
+                  </a>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
