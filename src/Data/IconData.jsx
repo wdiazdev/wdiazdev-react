0c7e4720499f669df9bc8c9e908e3ddd3587a89c
Wilfredo Diaz <wdiazdev@gmail.com>
2023-08-18 22:58:41 -0400

    Icons update

--- src/Data/IconData.jsx
+++ src/Data/IconData.jsx
@@ -1,71 +1,71 @@
-import * as FaIcons from 'react-icons/fa';
-import * as SiIcons from "react-icons/si";
+import * as FaIcons from "react-icons/fa"
+import * as SiIcons from "react-icons/si"
 
 const IconData = [
-    {
-        icon: <FaIcons.FaReact />,
-        name: 'React'
-    },
-    {
-        icon: <FaIcons.FaJsSquare />,
-        name: 'JavaScript'
-    },
-    {
-        icon: <FaIcons.FaNodeJs />,
-        name: 'Node.js'
-    },
-    {
-        icon: <SiIcons.SiTypescript />,
-        name: 'TypeScript'
-    },
-    {
-        icon: <FaIcons.FaCss3Alt />,
-        name: 'CSS3'
-    },
-    {
-        icon: <FaIcons.FaHtml5 />,
-        name: 'HTML5'
-    },
-    {
-        icon: <FaIcons.FaSass />,
-        name: 'SASS'
-    },
-    {
-        icon: <FaIcons.FaBootstrap />,
-        name: 'Bootstrap'
-    },
-    {
-        icon: <SiIcons.SiTailwindcss />,
-        name: 'Tailwind'
-    },
-    {
-        icon: <FaIcons.FaGitAlt />,
-        name: 'Git'
-    },
-    {
-        icon: <FaIcons.FaGithub />,
-        name: 'Github'
-    },
-    {
-        icon: <FaIcons.FaFigma />,
-        name: 'Figma'
-    },
-    {
-        icon: <FaIcons.FaPython />,
-        name: 'Python'
-    },
-    {
-        icon: <FaIcons.FaServer />,
-        name: 'Express.js'
-    },
-    {
-        icon: <FaIcons.FaDatabase />,
-        name: 'SQL'
-    },
-    {
-        icon: <SiIcons.SiPostgresql />,
-        name: 'PostgreSQL'
-    },
+  {
+    icon: <FaIcons.FaReact />,
+    name: "React",
+  },
+  {
+    icon: <FaIcons.FaJsSquare />,
+    name: "JavaScript",
+  },
+  {
+    icon: <FaIcons.FaNodeJs />,
+    name: "Node.js",
+  },
+  {
+    icon: <SiIcons.SiTypescript />,
+    name: "TypeScript",
+  },
+  {
+    icon: <FaIcons.FaCss3Alt />,
+    name: "CSS3",
+  },
+  {
+    icon: <FaIcons.FaHtml5 />,
+    name: "HTML5",
+  },
+  {
+    icon: <FaIcons.FaSass />,
+    name: "SASS",
+  },
+  {
+    icon: <FaIcons.FaBootstrap />,
+    name: "Bootstrap",
+  },
+  {
+    icon: <SiIcons.SiTailwindcss />,
+    name: "Tailwind",
+  },
+  {
+    icon: <FaIcons.FaGitAlt />,
+    name: "Git",
+  },
+  {
+    icon: <FaIcons.FaGithub />,
+    name: "Github",
+  },
+  {
+    icon: <FaIcons.FaFigma />,
+    name: "Figma",
+  },
+  {
+    icon: <FaIcons.FaPython />,
+    name: "Python",
+  },
+  {
+    icon: <FaIcons.FaServer />,
+    name: "Express.js",
+  },
+  {
+    icon: <SiIcons.SiAzuredevops />,
+    name: "Azure devops",
+  },
+  {
+    icon: <SiIcons.SiPostgresql />,
+    name: "PostgreSQL",
+  },
 ]
 
-export default IconData;
+export default IconData
